5780c8232663441a0965a48b0ee10a3421ede23c
Frankincense <frankincensewelsey@gmail.com>
2025-03-12 10:18:46 +0300

    array methods filtering arrays

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(arr, str){
+    let found = arr.filter(function(driver){
+        if (str.toLowerCase() === driver.toLowerCase()){
+            return driver;
+        }
+        
+    })
+    return found
+}
+
+function fuzzyMatch(arr, str){
+    let found = arr.filter(function(driver){
+        if (str === driver.substr(0,2)){
+            return driver
+        }
+    })
+    return found;
+}
+
+function matchName(arr, str){
+    let found = arr.filter(function(driver){
+        if (driver.name === str){
+            return driver
+        }
+    })
+    return found
+}
+console.log((drivers, "Bobby"))
